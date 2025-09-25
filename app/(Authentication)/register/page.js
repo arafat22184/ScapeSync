f267566d308fd614d497e046eb58dda9f0e3e299
arafat22184 <123alarafat@gmail.com>
2025-09-25 12:54:35 +0600

    add: Register page

--- app/(Authentication)/register/page.js
+++ app/(Authentication)/register/page.js
@@ -1,17 +1,17 @@
-import LoginForm from "../../components/Authentication/LoginForm";
+import RegisterForm from "../../components/Authentication/RegisterForm";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="max-w-[480px] mx-auto">
       <div className="px-2">
         <h1 className="text-[#212B36] text-2xl font-bold text-center mb-2">
-          Welcome to ScapeSync
+          Create your Account
         </h1>
         <p className="text-[#637381] text-center">
-          Please share your login details so you can access the website.
+          When sports Meets smart Tech.
         </p>
       </div>
-      <LoginForm></LoginForm>
+      <RegisterForm></RegisterForm>
     </div>
   );
 }
